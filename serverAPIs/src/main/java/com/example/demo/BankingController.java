e38e1a94010b60058ee9b6c75682c2fb75f8e8f1
shaniefrater <shaniemarie@live.co.uk>
2021-05-21 16:09:20 +0100

    Adding the server-side APIs to the application

--- serverAPIs/src/main/java/com/example/demo/BankingController.java
+++ serverAPIs/src/main/java/com/example/demo/BankingController.java
@@ -1,24 +1,18 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-
 @RestController
 public class BankingController {
-
-    @Autowired
-    private UsersRepository repository; // need help with this as I've created the validation but it's throwing errors :)
-
-    /** Basic permissions */
-
 
     /**
      * All HTTP Requests with regards to Transactions
@@ -31,15 +25,10 @@ public class BankingController {
     }
     // View Recent Transactions
 
-    @PostMapping("banking/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    String createTransaction(@RequestBody Banking transaction, HttpServletResponse response) {
-        if (1==repository.create(transaction)) {
-            return "{message:\"success\"}";
-        } else {
-            response.setStatus(404); // TODO - proper error handling
-            return "{error:\"not found\"}";
-        }
+    @PostMapping(value = {"banking/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    String createTransaction() {
+        return ("{\"message\":\"Create a transaction\"}");
     }
     // Creates New Transaction
 
@@ -54,26 +43,17 @@ public class BankingController {
     }
     // Obtains Standing Orders
 
-    @PostMapping("/standingOrders")
-    @ResponseStatus(HttpStatus.CREATED)
-    String createStandingOrder(@RequestBody Banking standingOrder, HttpServletResponse response) {
-        if (1==repository.create(standingOrder)) {
-            return "{message:\"standing order created\"}";
-        } else {
-            response.setStatus(404); // TODO - proper error handling
-            return "{error:\"standing order not created\"}";
-        }
+    @PostMapping(value = {"/standingOrder/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    String createStandingOrder() {
+        return ("{\"message\":\"Standing order created\"}");
     }
     // Creates New Standing Orders
 
-    @DeleteMapping("//standingOrder/{id}")
-    String deleteStandingOrder(@PathVariable String standingOrder,  HttpServletResponse response) {
-        if (1==repository.delete(standingOrder)) {
-            return "{message:\"Standing orders successfully deleted\"}";
-        } else {
-            response.setStatus(404); // TODO - proper error handling
-            return "{error:\"No standing orders found\"}";
-        }
-        // Creates New Standing Orders
+    @DeleteMapping(value = {"/standingOrder/{id}"},produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    String deleteStandingOrder() { return ("{message:\"Standing orders successfully deleted\"}");
     }
 }
+    // Creates New Standing Orders
+
