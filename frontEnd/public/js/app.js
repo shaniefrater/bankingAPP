67b86d8abf132c86acebde11f1b92a73ac35f53b
shaniefrater <shaniemarie@live.co.uk>
2021-05-21 16:10:01 +0100

    Editing the front end of the Banking App

---
+++ frontEnd/public/js/app.js
@@ -0,0 +1,162 @@
+// The Auth0 client, initialized in configureClient()
+let auth0 = null;
+
+/**
+ * Starts the authentication flow
+ */
+const login = async (targetUrl) => {
+  try {
+    console.log("Logging in", targetUrl);
+
+    const options = {
+      redirect_uri: window.location.origin
+    };
+
+    if (targetUrl) {
+      options.appState = { targetUrl };
+    }
+
+    await auth0.loginWithRedirect(options);
+  } catch (err) {
+    console.log("Log in failed", err);
+  }
+};
+
+const callApi = async () => {
+  try {
+
+    console.log("calling api");
+    // Get the access token from the Auth0 client
+    const accessToken = await auth0.getTokenSilently();
+    // const user = await auth0.getUser();
+    // console.log('token is:' +accessToken);
+    // console.log('user is:'+user);
+
+    // Make the call to the API, setting the token
+    // in the Authorization header
+    const response = await fetch('http://localhost:8080/banking', {
+      method: 'GET',
+      crossDomain: true,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+    // Fetch the JSON result
+    const responseData = await response.json();
+    console.log("response is:"+responseData);
+
+    
+    // TODO - hook up to UI
+
+} catch (e) {
+    // Display errors in the console
+    console.error(e);
+  }
+};
+
+/**
+ * Executes the logout flow
+ */
+const logout = () => {
+  try {
+    console.log("Logging out");
+    auth0.logout({
+      returnTo: window.location.origin
+    });
+  } catch (err) {
+    console.log("Log out failed", err);
+  }
+};
+
+/**
+ * Retrieves the auth configuration from the server
+ */
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+/**
+ * Initializes the Auth0 client
+ */
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+  audience: config.audience   // NEW - add the audience value
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+/**
+ * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+ * is prompted to log in
+ * @param {*} fn The function to execute if the user is logged in
+ */
+const requireAuth = async (fn, targetUrl) => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    return fn();
+  }
+
+  return login(targetUrl);
+};
+
+// Will run when page finishes loading
+window.onload = async () => {
+  await configureClient();
+
+  // If unable to parse the history hash, default to the root URL
+  if (!showContentFromUrl(window.location.pathname)) {
+    showContentFromUrl("/");
+    window.history.replaceState({ url: "/" }, {}, "/");
+  }
+
+  const bodyElement = document.getElementsByTagName("body")[0];
+
+  // Listen out for clicks on any hyperlink that navigates to a #/ URL
+  bodyElement.addEventListener("click", (e) => {
+    if (isRouteLink(e.target)) {
+      const url = e.target.getAttribute("href");
+
+      if (showContentFromUrl(url)) {
+        e.preventDefault();
+        window.history.pushState({ url }, {}, url);
+      }
+    }
+  });
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    console.log("> User is authenticated");
+    window.history.replaceState({}, document.title, window.location.pathname);
+    updateUI();
+    return;
+  }
+
+  console.log("> User not authenticated");
+
+  const query = window.location.search;
+  const shouldParseResult = query.includes("code=") && query.includes("state=");
+
+  if (shouldParseResult) {
+    console.log("> Parsing redirect");
+    try {
+      const result = await auth0.handleRedirectCallback();
+
+      if (result.appState && result.appState.targetUrl) {
+        showContentFromUrl(result.appState.targetUrl);
+      }
+
+      console.log("Logged in!");
+    } catch (err) {
+      console.log("Error parsing redirect:", err);
+    }
+
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  updateUI();
+};
